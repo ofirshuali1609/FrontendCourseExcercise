3545ace8e14e5a64e2e4c1f0ef40171b5853d7cb
ofirshuali1609 <115349516+ofirshuali1609@users.noreply.github.com>
2024-12-22 19:26:26 +0200

    fixed

--- Project_Part_Two/src/app/app.component.ts
+++ Project_Part_Two/src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { CounterComponent } from './counter/counter.component';
-
-@Component({
-  selector: 'app-root',
-  imports: [CounterComponent, RouterOutlet],
-=======
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
->>>>>>> 892f9fb29bd60cf1c6122b1c6791015e42bafcdb
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
