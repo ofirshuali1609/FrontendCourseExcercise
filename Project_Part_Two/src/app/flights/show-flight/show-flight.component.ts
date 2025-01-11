8ed07fbb4cb5afcb21970366fff375eb2d288871
ofirshuali1609 <115349516+ofirshuali1609@users.noreply.github.com>
2025-01-11 17:48:16 +0200

    Fix Forms

--- Project_Part_Two/src/app/flights/show-flight/show-flight.component.ts
+++ Project_Part_Two/src/app/flights/show-flight/show-flight.component.ts
@@ -1,18 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Flight } from '../../model/flight';
 import { FlightsService } from '../../service/flights/flights.service';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-show-flight',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule,MatFormFieldModule,MatNativeDateModule,MatDatepickerModule,MatInputModule,FormsModule,MatSelectModule],
   templateUrl: './show-flight.component.html',
   styleUrl: './show-flight.component.css'
 })
 export class ShowFlightComponent implements OnInit {
-  @Input() flightNo ='0'; 
-  flight !: Flight | undefined; 
+  @Input() flightNo ='0';
+  flight !: Flight | undefined;
 
-  constructor(private flightService: FlightsService) { 
+  constructor(private flightService: FlightsService) {
   }
   ngOnInit(): void {
     this.flight = this.flightService.get(this.flightNo);  }
